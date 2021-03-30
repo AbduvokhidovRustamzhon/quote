c85a980e1143c272411bf84b4e7dd415917f0f53
Rustam <abduvokhidov.rb@phystech.edu>
2021-03-30 07:51:04 +0500

    add "quote" model

---
+++ pkg/model/quote.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Quote struct {
+	ID        string    `json:"id"`
+	Author    string    `json:"author"`
+	Quote     string    `json:"quote"`
+	Category  string    `json:"category"`
+	CreatedAt time.Time `json:"created_at"`
+}
